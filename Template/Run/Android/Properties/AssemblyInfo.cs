9d4e85606e09dc3ba1ef97bb507a1b109ee73173
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2020-11-23 12:49:04 +0330

    Update template

--- Template/Run/Android/Properties/AssemblyInfo.cs
+++ Template/Run/Android/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("$SolutionName$")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © $year$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- Template/Run/UWP/Properties/AssemblyInfo.cs
+++ Template/Run/UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("$SolutionName$")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © $year$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
