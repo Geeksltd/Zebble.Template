02f853dd28fc0d9b7c62ea550d16adf258bc6108
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-11-27 18:31:11 +0330

    Add UI testing

---
+++ Template/App.UI/Tests/TestCases/SampleTest.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Zebble.Testing;
+
+namespace UI.Tests
+{
+    //[UnderDevelopment]
+    public class SampleTest : UITest
+    {
+        public override async Task Run()
+        {
+            //TODO : ....
+        }
+    }
+}
