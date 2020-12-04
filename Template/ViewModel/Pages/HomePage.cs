63d214cfd2c51c7f7fff205eb9396010db400819
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2020-12-04 13:22:43 +0330

    Update template

--- Template/ViewModel/Pages/HomePage.cs
+++ Template/ViewModel/Pages/HomePage.cs
@@ -1,6 +1,40 @@
 ﻿namespace ViewModel
 {
+    using Domain;
+    using System.Threading.Tasks;
+    using Zebble;
     using Zebble.Mvvm;
 
-    class HomePage : FullScreen { }
+    class HomePage : FullScreen
+    {
+        readonly IAuthService AuthService;
+
+        public readonly Bindable<bool> IsBusy = new(false);
+        public readonly Bindable<string> LoggedInUserEmail = new("joe@doe.com");
+
+        public HomePage()
+        {
+            AuthService = new FirebaseAuthService();
+        }
+
+        protected override async Task NavigationStartedAsync()
+        {
+            IsBusy.Set(true);
+
+            var user = await AuthService.GetUser();
+
+            LoggedInUserEmail.Set(user.Email);
+
+            IsBusy.Set(false);
+
+            await base.NavigationStartedAsync();
+        }
+
+        public async Task TapLogout()
+        {
+            await AuthService.Logout();
+
+            Go<WelcomePage>(PageTransition.SlideBack);
+        }
+    }
 }
---
+++ Template/ViewModel/Pages/LatestShoesPage.cs
@@ -0,0 +1,34 @@
+﻿namespace ViewModel
+{
+    using Domain.Extensions;
+    using Domain;
+    using Zebble.Mvvm;
+    using System.Threading.Tasks;
+    using Zebble;
+
+    class LatestShoesPage : FullScreen
+    {
+        readonly IShoeService ShoeService;
+
+        public readonly Bindable<bool> IsBusy = new(false);
+        public readonly CollectionViewModel<ShoeList.Item> Items = new();
+
+        public LatestShoesPage()
+        {
+            ShoeService = new FirebaseShoeService();
+        }
+
+        protected override async Task NavigationStartedAsync()
+        {
+            IsBusy.Set(true);
+
+            var shoes = await ShoeService.GetLatestShoes();
+
+            shoes.ForEach(Items.Add);
+
+            IsBusy.Set(false);
+
+            await base.NavigationStartedAsync();
+        }
+    }
+}
