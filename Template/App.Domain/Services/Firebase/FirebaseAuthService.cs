db6927da3eccfa69776fbabf9cf82924d4ab4e92
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2020-12-07 19:02:19 +0330

    Add Google Firebase Auth creation instructions

--- Template/App.Domain/Services/Firebase/FirebaseAuthService.cs
+++ Template/App.Domain/Services/Firebase/FirebaseAuthService.cs
@@ -9,6 +9,19 @@
     using Zebble;
     using Zebble.Device;
 
+    /// <summary>
+    /// This implementation is provided to let the users log in to their accounts,
+    /// or if they haven't created one before, they'll be able to create a new account.
+    /// You, as the developer of this project, need to set up a Google Firebase Auth app in your Console.
+    /// To create a new project, open your Firebase console(https://console.firebase.google.com),
+    /// click on Add project, enter a name, and then click on the Create Project button.
+    /// After that, you've to add an Authentication app to your project.
+    /// To do so, on the right side of the project's overview page, head to Develop > Authentication > Get Started.
+    /// In this step, you can configure your app to use one or more of the available sign-in providers.
+    /// Click on Email/Password, turn the Enable switch on, and click on the Save button.
+    /// Next, you have to copy your API token by heading to Project settings > General > Web API Key and then add it to your Config.xml file as follow:
+    /// <Firebase.ApiToken value="<YOUR_API_TOKEN>" />
+    /// </summary>
     class FirebaseAuthService : IAuthService
     {
         public async Task<bool> ValidateUserValidity()
@@ -167,7 +180,7 @@
             var apiToken = Config.Get<string>("Firebase.ApiToken");
 
             if (!apiToken.HasValue())
-                throw new Exception("To use Google Firebase Auth service you'll need to add the provided API token to the Config.xml file.");
+                throw new Exception("To use Google Firebase Auth service you'll need to add the provided API token to the Config.xml file. For more info, check out the FirebaseAuthService.");
 
             return REQUEST_PATH.FormatWith(path, apiToken);
         }
