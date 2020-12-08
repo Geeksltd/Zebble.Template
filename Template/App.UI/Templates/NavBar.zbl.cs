5e53a66d086a05c47325bbba76d7dacd208bd097
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-08 14:06:05 +0330

    Add SafeAera (T13067)

--- Template/App.UI/Templates/NavBar.zbl.cs
+++ Template/App.UI/Templates/NavBar.zbl.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Zebble;
 
 namespace UI.Templates
@@ -5,5 +7,13 @@ namespace UI.Templates
     partial class NavBar
     {
         public string Title { get => Bar.Title.Text; set => Bar.Title.Text(value); }
+
+        public override async Task OnPreRender()
+        {
+            await base.OnPreRender();
+
+            BodyScrollerWrapper.Y.BindTo(Bar.Height, SafeAreaContainer.Padding.Top, (h, p) => h + p);
+            BodyScrollerWrapper.Height.BindTo(SafeAreaContainer.Height, Bar.Height, Tabs.Current.Height, SafeAreaContainer.Padding.Top, (vh, nh, ts, st) => vh - nh - st - ts);
+        }
     }
 }
