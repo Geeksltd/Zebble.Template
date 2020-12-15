1b97098c2a49631ea4820e6189fd613d1665c785
Geeksltd <master@geeks.ltd.uk>
2020-12-15 18:48:55 +0000

    TDD

--- Template/Run/VM/DevContext.cs
+++ Template/Run/VM/DevContext.cs
@@ -5,14 +5,17 @@ using System.Collections.Generic;
 using System.Text;
 using ViewModel;
 using Zebble.Mvvm;
+using System.Threading.Tasks;
 
-namespace VM
+namespace App
 {
-    class Script : TestScript
+    abstract class DevContext : TestScript
     {
-        public static void Run()
+        /// <summary>
+        /// Run quick steps to get you to the context you're developing.
+        /// </summary>
+        public static async Task Reach()
         {
-            // Run quick steps to get you to the context you're developing.
             //On<WelcomePage>().TapLogin();
             //On<LoginPage>().TapLogin();
             //On<ShoesPage>().Items[0].Tap();
