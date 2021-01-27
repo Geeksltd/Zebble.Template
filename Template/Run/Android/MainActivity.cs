266778bff92f00b7046da0af1610526e5c6b9d84
PaymonK <35069374+PaymonK@users.noreply.github.com>
2021-01-27 10:12:27 +0000

    Update MainActivity.cs

--- Template/Run/Android/MainActivity.cs
+++ Template/Run/Android/MainActivity.cs
@@ -30,5 +30,11 @@
 
             await (StartUp.Current = new UI.StartUp()).Run();
         }
+        
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
