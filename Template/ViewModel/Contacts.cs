ee43f9fd55d98f16cbbcdbd286d4b632a782b684
Me <mahsa.ghasemzadeh@geeks.ltd.uk>
2021-04-16 17:15:17 +0100

    Add view models and pages and domain models for three sample menu

---
+++ Template/ViewModel/Contacts.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Olive;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Zebble.Mvvm;
+
+namespace ViewModel
+{
+	public class Contacts : FullScreen
+	{
+		public CollectionViewModel<Item> Items = new();
+
+
+		public class Item : Zebble.Mvvm.ViewModel<Contact>
+		{
+			public Bindable<string> FirstName => Source.Get(x => x.FirstName);
+			public Bindable<string> LastName => Source.Get(x => x.LastName);
+			public Bindable<byte[]> Photo => Source.Get(x => x.Photo);
+			public Bindable<string> Phone => Source.Get(x => x.PhoneNumber);
+			public Bindable<DateTime> DateOfBirth => Source.Get(x => x.DateOfBirth.Value);
+			public Bindable<string> Email
+		}
+
+	}
+}
---
+++ Template/ViewModel/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zebble.Mvvm;
+
+namespace ViewModel
+{
+	public class Settings : FullScreen
+	{
+	}
+}
